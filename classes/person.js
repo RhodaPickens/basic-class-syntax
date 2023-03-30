8b72f74f91a0e176e835892b5010db5c8245bbb3
Rhoda Pickens <rhodajane@gmail.com>
2023-03-30 13:46:54 -0500

    Created constructor and methods

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,35 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  // instance method
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  // static method
+  static introducePeople(arr) {
+
+    if(Array.isArray(arr) === false) {
+      console.log("introducePeople only takes an array as an argument.");
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      let people = arr[i];
+      if (people instanceof Person === false) {
+        console.log("All items in array must be Person class instances.");
+      }
+      else {
+        people.introduce();
+      }
+    }
+  }
+
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
